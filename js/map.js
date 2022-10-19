4f5d566542655f5168c92062fd1da02f1f060483
Bizzare-Giovanni <lumaynopedro33@gmail.com>
2022-10-20 01:57:42 +0800

    Updated map view to be dynamic

--- js/map.js
+++ js/map.js
@@ -1,17 +1,20 @@
-var map = L.map('map').setView([10.6840, 122.9563], 17);
+var dArrayLatLong = [];
+console.log(arrayLatLong);
+for (let i = 0; i < 2; i++) {
+    dArrayLatLong[i] = [];
+    
+    dArrayLatLong[i][0] = arrayLatLong[i].coordLong;
+    dArrayLatLong[i][1] = arrayLatLong[i].coordLat;
+}
+
+var map = L.map('map').setView(dArrayLatLong[0], 17);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var dArrayLatLong = [];
-for (let i = 0; i < 1; i++) {
-    dArrayLatLong[i] = [];
-    for (let j = 0; j < 2; j++) {
-        dArrayLatLong[i][j] = arrayLatLong[j];
-    }
-}
+console.log(dArrayLatLong);
 
 for (let i = 0; i < dArrayLatLong.length; i++) {
     L.marker(dArrayLatLong[i]).addTo(map)
