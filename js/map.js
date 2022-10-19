9980a934c62cd8cb296b764ce1ee1ddc41f36fc4
Bizzare-Giovanni <lumaynopedro33@gmail.com>
2022-10-20 00:25:36 +0800

    Added basic popup functionality

--- js/map.js
+++ js/map.js
@@ -14,5 +14,7 @@ for (let i = 0; i < 1; i++) {
 }
 
 for (let i = 0; i < dArrayLatLong.length; i++) {
-    L.marker(dArrayLatLong[i]).addTo(map);
+    L.marker(dArrayLatLong[i]).addTo(map)
+            .bindPopup('<b>Hello World</b>')
+            .openPopup();
 }
